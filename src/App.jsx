d89831568aa41f149b2a9c6b2645c12bd6add90e
BelliaNino <bellianino1996@gmail.com>
2026-06-12 10:41:41 +0200

    fixed route notfound

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,15 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Structure/>}>
-        <Route index element={<Navigate to='/Homepage' replace />}/>
-          <Route path='/Homepage' element={<Homepage/>}/>
-          <Route path='/Show' element={<Show/>}/>
-          <Route path='/ProductDetail' element={<ProductDetail/>}/>
-        </Route>
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Structure />}>
+            <Route index element={<Navigate to='/Homepage' replace />} />
+            <Route path='/Homepage' element={<Homepage />} />
+            <Route path='/Show' element={<Show />} />
+            <Route path='/ProductDetail' element={<ProductDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   )
